df35c6f92b0772591f9826759720778615039120
MorganGui <morgan.guillemin1@gmail.com>
2023-06-18 16:46:50 +0200

    fix checkOwn

--- middlewares/checkOwnOrAdmin.js
+++ middlewares/checkOwnOrAdmin.js
@@ -1,5 +1,5 @@
 const checkOwnOrAdmin = (req, res, next) => {
-  if (req.user.id === req.params.id || req.user.role === 'admin') {
+  if (req.user.id === parseFloat(req.params.id) || req.user.role === 'admin') {
     next()
   } else {
     res.status(401).end('unauthorized')
